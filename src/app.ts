5ac63786534bbcf9c735ec300a36785a68ad1b23
Hyeonki Hong <hhk7734@gmail.com>
2023-06-25 22:47:30 +0900

    feat(app): add PR commands handler

--- src/app.ts
+++ src/app.ts
@@ -13,4 +13,48 @@ export function route(app: Probot) {
 			action,
 		});
 	});
+
+	app.on("issue_comment.created", async (context) => {
+		// Commands for Pull Requests
+		if (context.payload.issue.pull_request) {
+			const repoName = context.payload.repository.name;
+			const repoOwner = context.payload.repository.owner.login;
+			const comment = context.payload.comment.body;
+			const commentUser = context.payload.comment.user.login;
+
+			const commands = comment
+				.split("\n")
+				.filter((line) => line.startsWith("/"))
+				.map((line) => line.split(" "));
+
+			if (commands.length === 0) {
+				return;
+			}
+
+			const collaboratorPermissionLevel =
+				await context.octokit.repos.getCollaboratorPermissionLevel({
+					owner: repoOwner,
+					repo: repoName,
+					username: commentUser,
+				});
+
+			const commentUserRole = collaboratorPermissionLevel.data.role_name;
+
+			if (!["admin", "maintain", "write"].includes(commentUserRole)) {
+				const comment = context.issue({
+					body: `commands are only available to people with Admin || Maintainer || Write role on ${repoName}.`,
+				});
+				await context.octokit.issues.createComment(comment);
+				return;
+			}
+
+			for (const c of commands) {
+				const command = c[0];
+				const args = c.slice(1);
+
+				switch (command) {
+				}
+			}
+		}
+	});
 }
